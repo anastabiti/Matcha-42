f39f61835f84759269ef718c1da2d3b9803b8738
anastabiti <anastabiti@gmail.com>
2025-01-17 13:27:31 +0100

    working on password reset

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -8,6 +8,7 @@ const bcrypt = require("bcrypt");
 const authRouter = express.Router();
 const crypto = import("crypto");
 import nodemailer from "nodemailer";
+import { auth } from "neo4j-driver-core";
 
 const transporter = nodemailer.createTransport({
   service: "Gmail",
@@ -89,31 +90,35 @@ authRouter.post(
 
     if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
     else {
-        try {
-      console.log(req.body.email, " email");
-      const email = req.body.email;
-      if (email) {
-        if (session) {
-          const url_token = jwt.sign({email: email}, process.env.JWT_TOKEN_SECRET, { expiresIn: "10min" });
+      try {
+        console.log(req.body.email, " email");
+        const email = req.body.email;
+        if (email) {
+          if (session) {
+            const url_token = jwt.sign(
+              { email: email },
+              process.env.JWT_TOKEN_SECRET,
+              { expiresIn: "10min" }
+            );
 
-          const res_ = await session.run(
-            `MATCH (n:User) WHERE n.email = $email
+            const res_ = await session.run(
+              `MATCH (n:User) WHERE n.email = $email
              SET n.password_reset_token = $url_token 
              RETURN n.email`,
-            { email: email, url_token: url_token }
-          );
-          console.log(res_.records, " res_");
-          if (res_.records.length > 0) {
-            console.log(
-              "password reset successful, check your email for further instructions"
+              { email: email, url_token: url_token }
             );
-            ///////////////////
+            console.log(res_.records, " res_");
+            if (res_.records.length > 0) {
+              console.log(
+                "password reset successful, check your email for further instructions"
+              );
+              ///////////////////
 
-            const mailOptions = {
-              from: "anastabiti@gmail.com",
-              to: email,
-              subject: "Reset Your password ,Tinder! 💖",
-              text: `Hi ${email},
+              const mailOptions = {
+                from: "anastabiti@gmail.com",
+                to: email,
+                subject: "Reset Your password ,Tinder! 💖",
+                text: `Hi ${email},
         
         Welcome use the link below to reset your password! 🎉        
         🔗 Reset Your Password: http://localhost:3000/api/reset_it?token=${url_token}
@@ -121,31 +126,55 @@ authRouter.post(
         
         Best regards,  
         The Tinder Team`,
-            };
+              };
 
-            transporter.sendMail(mailOptions, (error, info) => {
-              if (error) {
-                console.error("Error sending email: ", error);
-              } else {
-                console.log("Email sent: password reset ", info.response);
-              }
-            });
+              transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                  console.error("Error sending email: ", error);
+                } else {
+                  console.log("Email sent: password reset ", info.response);
+                }
+              });
 
-            res.send(
-              "password reset successful, check your email for further instructions"
-            );
+              res.send(
+                "password reset successful, check your email for further instructions"
+              );
+            }
+          } else {
+            console.log("email does not exist");
+            res.status(400).send("email does not exist");
           }
-        } else {
-          console.log("email does not exist");
-          res.status(400).send("email does not exist");
         }
+      } catch (Error) {
+        console.log("error");
+        res.status(400).send("Error in password reset");
       }
-        } catch (Error) {
-          console.log("error");
-          res.status(400).send("Error in password reset");
-        }
     }
   }
 );
+
+// 
+authRouter.get("/reset_it", 
+// authRouter.patch("/reset_it",   body("password").isLength({ min: 6, max: 30 }),
+async (req: Request, res: Response) => {
+  // console.log(req.query.token, " token");
+//   console.log(req.body.password, "password");
+  try {
+    const token = req.query.token;
+    if (token) {
+      const jwt_ = await jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+
+      console.log(jwt_, " jwt_");
+
+      res.send("password reset api is called");
+    }
+
+    else //empty token
+    res.status(400).send("invalid token");
+  } catch {
+    console.log("error in reset_it");
+    res.status(400).send("Expired or invalid token");
+  }
+});
 
 export default authRouter;
