e1d7578d50636f614d3a9cc73e32cbb2cdef85f2
anastabiti <anastabiti@gmail.com>
2025-02-03 11:59:39 +0100

    working on gps

--- backend/index.ts
+++ backend/index.ts
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:7070",
-    origin:process.env.front_end_ip,
+    origin: process.env.front_end_ip,
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -33,21 +33,19 @@ io.on("connection", async (socket: any) => {
   }
   console.log(jwt_token, "-------jwt_token----------\n\n");
   if (jwt_token) {
-    const decoded :any= await jwt.verify(jwt_token, process.env.JWT_TOKEN_SECRET as string);
+    const decoded: any = await jwt.verify(jwt_token, process.env.JWT_TOKEN_SECRET as string);
     console.log("-------------------------------------------------------");
     console.log(decoded.username, " decoded.username ----------------=-\n\n\n\n");
     console.log("-------------------------------------------------------");
     socket.join(decoded.username);
     // Get all socket IDs in the room
-    io.in(decoded.username).fetchSockets().then((sockets) => {
-      const socketIds = sockets.map(s => s.id);
-      console.log(`Sockets in room ${decoded.username}:`, socketIds);
-  });
+    io.in(decoded.username)
+      .fetchSockets()
+      .then((sockets) => {
+        const socketIds = sockets.map((s) => s.id);
+        console.log(`Sockets in room ${decoded.username}:`, socketIds);
+      });
   }
-
-
-
-
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
@@ -64,9 +62,34 @@ io.on("connection", async (socket: any) => {
 
     // Emit to all connected clients (including sender)
     // io.emit("newMessage", newMessage);
-    socket.to('atabiti').emit("newMessage", newMessage);
-
+    socket.to("atabiti").emit("newMessage", newMessage);
   });
+});
+
+const axios = require("axios");
+
+
+app.get("/location_specific_service", async (req: any, res: any) => {
+  //get public ip first
+
+
+    const response = await axios.get("http://api.ipify.org");
+    console.log(response.data, " , -------res");
+    
+  const pub_ip = response.data
+  console.log(
+    req.ip,
+    " ip --------------------------------------------\n\n\n\n",
+    req.socket.remoteAddress,
+    "}}}]",
+    req.headers["x-forwarded-for"]?.split(",")[0]
+  );
+
+  const url = `https://apiip.net/api/check?ip=${pub_ip}&accessKey=${process.env.ip_finder_pub}`;
+  const responses = await axios.get(url);
+  const result = responses.data;
+  console.log(result, "sssss---------------result");
+
 });
 
 server.listen(port, () => {
