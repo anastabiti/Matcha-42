fa8557593f0c07004af6baa1652388f854f7c3a7
anastabiti <anastabiti@gmail.com>
2025-02-03 10:42:04 +0100

    Move gender into User node Add fame_rating and age properties Refactor user pictures into pics array

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -402,16 +402,13 @@ user_information_Router.post(
         });
 
         // Handle profile picture (first image)
-        if (!i) {
+        if (index === 0) {
           existingPics[index] = ret.url
           await session.run(
             `MATCH (u:User {username: $username})
-             SET u.profile_picture = $profile_picture
-            SET u.pics = $pics
-               u.pics = $pics
-
+              SET u.profile_picture = $profile_picture
              RETURN u`,
-            { username: _user.username, profile_picture: ret.url , pics: existingPics }
+            { username: _user.username, profile_picture: ret.url}
           );
         }
 
