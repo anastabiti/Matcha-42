3342cd6c5571255ba5a30e4342a1c5f0ce287b52
anastabiti <anastabiti@gmail.com>
2025-02-21 14:32:17 +0100

    fix warnings  Some chunks are larger than 500 kB after minification. Consider: ,Module level directives cause errors when bundled, use client in node_modul , update vitejs/plugin-react

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+  
 })
 // export default defineConfig({
 //   //   plugins: [react()],
@@ -13,3 +17,10 @@ export default defineConfig({
 //     // other configuration...
 //   }
 // });
+
+/*    chunkSizeWarningLimit: 1600,
+ 
+(!) Some chunks are larger than 500 kB after minification. Consider:
+- Using dynamic import() to code-split the application
+- Use build.rollupOptions.output.manualChunks to improve chunking: https://rollupjs.org/configuration-options/#output-manualchunks
+- Adjust chunk size limit for this warning via build.chunkSizeWarningLimit.*/
